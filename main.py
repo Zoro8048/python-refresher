a723db03a63db8682dc7c764630482d332ce8562
Keerthan Nandigama <nandigama.keerthan@cars24.com>
2023-07-03 23:28:33 +0530

    check

---
+++ main.py
@@ -0,0 +1,42 @@
+# Int
+my_int = 7
+print(my_int)   # Outputs 7
+
+# Float
+my_float = 7.0
+print(my_float)  # Outputs 7.0
+
+my_float = float(7)
+print(my_float)  # Outputs 7.0
+
+
+# Strings
+my_string = 'hello'
+print(my_string)  # Outputs hello
+
+my_string = "hello"
+print(my_string)  # Outputs hello
+
+
+one = 1
+two = 2
+three = one + two
+print(three)  # Outputs 3
+
+
+hello = "hello"
+world = "world"
+hello_world = hello + " " + world
+print(hello_world)  # Outputs hello world
+
+
+# assignments can be done more than one variable on the same line
+a, b = 3, 4
+print(a, b)  # Outputs 3 4
+
+# mixing operators between numbers and strings is not supported:
+one = 1
+two = 2
+hello = "hello"
+print(one + two + hello)    # throws error
+

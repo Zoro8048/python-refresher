3e5e085cd52392aeb7d3c63e4678e93c5059a56c
Keerthan Nandigama <nandigama.keerthan@cars24.com>
2023-07-04 11:52:16 +0530

    string formatting

--- main.py
+++ main.py
@@ -1,26 +1,19 @@
-number = 1 + 2 * 3 / 4.0
-print(number) # prints 2.5
+name = "Keerthan"
+print("Hello, %s!" % name)  # prints "Hello, Keerthan!"
 
-remainder = 11 % 3
-print(remainder) # prints 2
+age = 24
+print("%s is %d years old" %(name, age))
 
-squared = 7 ** 2
-cubed = 2 ** 3
-print(squared)  # prints 49
-print(cubed)    # prints 8
-
-hello_world = "hello" + " " + "world"
-print(hello_world)      # prints "hello world"
-
-lots_of_hellos = "hello" * 10
-print(lots_of_hellos)   # prints "hello" 10 times
+my_list = [1, 2, 3]
+print("list: %s" % my_list)
 
 
-even_numbers = [2, 4, 6, 8]
-odd_numbers = [1, 3, 5, 7]
-all_numbers = odd_numbers + even_numbers
-print(all_numbers)      # prints [1, 3, 5, 7, 2, 4, 6, 8]
+# %s - String (or any object with a string representation, like numbers)
 
+# %d - Integers
 
-number_list = [1, 2, 3]
-print(number_list * 3)  # prints number_list thrice
+# %f - Floating point numbers
+
+# %.<number of digits>f - Floating point numbers with a fixed amount of digits to the right of the dot.
+
+# %x/%X - Integers in hex representation (lowercase/uppercase)

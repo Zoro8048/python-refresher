b1735a53ff8de79afdb8dc3859ab9c0e9d6f25af
Keerthan Nandigama <nandigama.keerthan@cars24.com>
2023-07-04 11:24:44 +0530

    lists

--- main.py
+++ main.py
@@ -1,42 +1,10 @@
-# Int
-my_int = 7
-print(my_int)   # Outputs 7
+my_list = []
+my_list.append(1)
+my_list.append(2)
+my_list.append(3)
+print(my_list[0]) # prints 1
+print(my_list[1]) # prints 2
+print(my_list[2]) # prints 3
 
-# Float
-my_float = 7.0
-print(my_float)  # Outputs 7.0
-
-my_float = float(7)
-print(my_float)  # Outputs 7.0
-
-
-# Strings
-my_string = 'hello'
-print(my_string)  # Outputs hello
-
-my_string = "hello"
-print(my_string)  # Outputs hello
-
-
-one = 1
-two = 2
-three = one + two
-print(three)  # Outputs 3
-
-
-hello = "hello"
-world = "world"
-hello_world = hello + " " + world
-print(hello_world)  # Outputs hello world
-
-
-# assignments can be done more than one variable on the same line
-a, b = 3, 4
-print(a, b)  # Outputs 3 4
-
-# mixing operators between numbers and strings is not supported:
-one = 1
-two = 2
-hello = "hello"
-print(one + two + hello)    # throws error
-
+for x in my_list:
+        print(x)

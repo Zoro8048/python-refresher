3f705159cb9b35131928cca9693986c5d96eaeab
Keerthan Nandigama <nandigama.keerthan@cars24.com>
2023-07-04 11:45:56 +0530

    basic operations

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-my_list = []
-my_list.append(1)
-my_list.append(2)
-my_list.append(3)
-print(my_list[0]) # prints 1
-print(my_list[1]) # prints 2
-print(my_list[2]) # prints 3
+number = 1 + 2 * 3 / 4.0
+print(number) # prints 2.5
 
-for x in my_list:
-        print(x)
+remainder = 11 % 3
+print(remainder) # prints 2
+
+squared = 7 ** 2
+cubed = 2 ** 3
+print(squared)  # prints 49
+print(cubed)    # prints 8
+
+hello_world = "hello" + " " + "world"
+print(hello_world)      # prints "hello world"
+
+lots_of_hellos = "hello" * 10
+print(lots_of_hellos)   # prints "hello" 10 times
+
+
+even_numbers = [2, 4, 6, 8]
+odd_numbers = [1, 3, 5, 7]
+all_numbers = odd_numbers + even_numbers
+print(all_numbers)      # prints [1, 3, 5, 7, 2, 4, 6, 8]
+
+
+number_list = [1, 2, 3]
+print(number_list * 3)  # prints number_list thrice
